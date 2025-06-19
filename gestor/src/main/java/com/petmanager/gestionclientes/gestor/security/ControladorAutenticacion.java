7b2a651d9f4a623c835117ddb84574993036aa79
Dorian88 <doaljari@gmail.com>
2025-06-19 12:00:05 -0500

    Final

---
+++ gestor/src/main/java/com/petmanager/gestionclientes/gestor/security/ControladorAutenticacion.java
@@ -0,0 +1,33 @@
+package com.petmanager.gestionclientes.gestor.security;
+
+import com.petmanager.gestionclientes.gestor.model.Usuario;
+import com.petmanager.gestionclientes.gestor.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/auth")
+public class ControladorAutenticacion {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody Usuario fechaLoguin){
+        Optional<Usuario> usuario = usuarioRepository.findAll().stream()
+                .filter(u -> u.getCorreo_electronico().equals(fechaLoguin.getCorreo_electronico()) && u.getContrasena().equals(fechaLoguin.getContrasena()))
+                .findFirst();
+
+        if(usuario.isPresent()){
+            String token = jwtUtil.generarToken(usuario.get().getCorreo_electronico(), usuario.get().getRol());
+            return ResponseEntity.ok().body("Bearer " + token);
+        }else{
+            return ResponseEntity.status(401).body("credenciales incorrectas");
+        }
+    }
+}
